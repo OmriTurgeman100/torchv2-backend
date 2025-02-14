806fcf19b83dac306d31b87a21c41d30323b6a3c
omri <omrigamer100@gmail.com>
2025-02-14 03:35:37 +0200

    test

--- src/controller/report_nodes_controller.ts
+++ src/controller/report_nodes_controller.ts
@@ -3,11 +3,8 @@ import pool from "../database/database";
 import AppError from "../utils/AppError";
 import { CatchAsync } from "../utils/CatchAsync";
 
-export const test = async (req: Request, res:Response, next: NextFunction): Promise<void> => {
-  try {
-    res.status(200).json("test route")
-
-  } catch (error) {
-    console.log(error);
+export const test = CatchAsync(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    res.status(200).json("test route");
   }
-};
+);

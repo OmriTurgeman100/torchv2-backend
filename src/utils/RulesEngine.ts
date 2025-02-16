13e798d6fe4ab202ebf9a440a30da4dcf5ff4994
omri <omrigamer100@gmail.com>
2025-02-16 15:51:10 +0200

    testtt

--- src/utils/RulesEngine.ts
+++ src/utils/RulesEngine.ts
@@ -10,67 +10,106 @@ class RulesEngine {
   }
 
   async StartRulesEngine(): Promise<void> {
+    while (this.parent != null) {
+      const rule = await pool.query(
+        "select * from rules where parent_node_id = $1;",
+        [this.parent]
+      );
 
-    while (this.parent != null ) {
-        const rule = await pool.query("select * from rules where parent_node_id = $1;", [this.parent])
-        
+      let rules_data = rule.rows[0].conditions[0];
 
-        let rules_data = rule.rows[0].conditions[0]
+      // console.log(rules_data)
 
-        // console.log(rules_data)
+      // console.log(`parent is ${this.parent}`)
 
-        console.log(`parent is ${this.parent}`)
+      if (rules_data.report_id) {
+        // console.log('eval report rules')
 
-        if (rules_data.report_id) {
-            // console.log('eval report rules')
-
-            await this.Evaluate_Report_Rules()
-
-        } else if (rules_data.node_id) {
-            console.log('eval node rules')
-            await this.Evaluate_Node_Rules()
-
-        }
-
+        await this.Evaluate_Report_Rules();
+      } else if (rules_data.node_id) {
+        // console.log('eval node rules')
+        await this.Evaluate_Node_Rules();
+      }
     }
 
     if (this.parent == null) {
-        console.log(`loop ended, parent is ${this.parent}, ${typeof this.parent}`)
-       
+      console.log(
+        `loop ended, parent is ${this.parent}, ${typeof this.parent}`
+      );
     }
   }
 
-
   async Evaluate_Report_Rules(): Promise<void> {
+    const report = await pool.query(
+      "SELECT DISTINCT ON (report_id) report_id, parent, title, description, value, excluded, time FROM reports WHERE parent = $1 ORDER BY report_id, time DESC;",
+      [this.parent]
+    );
 
-    console.log(`hey from eval report rules parent is ${this.parent}`)
+    const report_value: number = report.rows[0].value;
 
-    const report_rule = await pool.query("select * from rules where parent_node_id = $1;", [this.parent])
+    const report_rule = await pool.query(
+      "select * from rules where parent_node_id = $1;",
+      [this.parent]
+    );
 
-    console.log(`report rules are`)
+    for (const rule of report_rule.rows) {
+      const operator = rule.operator;
 
-    console.log(report_rule.rows)
+      const action = rule.action;
 
-    const recursion = await pool.query("select * from nodes where node_id = $1;", [this.parent])
+      for (const condition of rule.conditions) {
+        switch (operator) {
+          case ">":
+            if (report_value > condition.threshold) {
+              console.log(
+                `matched found ${report_value}, t ${condition.threshold}`
+              );
+              console.log(1);
+            }
+            break;
+          case "<":
+            if (report_value < condition.threshold) {
+              console.log(
+                `matched found ${report_value}, t ${condition.threshold}`
+              );
+              console.log(2);
+            }
+            break;
+          case "==":
+            if (report_value == condition.threshold) {
+              console.log(
+                `matched found ${report_value}, t ${condition.threshold}`
+              );
+              console.log(3);
+            }
+            break;
+          default:
+            null;
+        }
+      }
+    }
 
-    console.log(recursion.rows[0].parent)
+    const recursion = await pool.query(
+      "select * from nodes where node_id = $1;",
+      [this.parent]
+    );
 
-
-    this.parent = recursion.rows[0].parent
-
+    this.parent = recursion.rows[0].parent;
   }
 
   async Evaluate_Node_Rules(): Promise<void> {
-    // console.log('hey from eval nodes')
-    
-    const node_rule = await pool.query("select * from rules where parent_node_id = $1;", [this.parent])
+    const node_rule = await pool.query(
+      "select * from rules where parent_node_id = $1;",
+      [this.parent]
+    );
 
-    const recursion = await pool.query("select * from nodes where node_id = $1;", [this.parent])
+    const recursion = await pool.query(
+      "select * from nodes where node_id = $1;",
+      [this.parent]
+    );
 
-    this.parent = recursion.rows[0].parent
-
+    this.parent = recursion.rows[0].parent;
   }
 }
-
 
 export default RulesEngine;

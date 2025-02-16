fa94f969c670b29fef1aee42f5303bfcfc621cb4
omri <omrigamer100@gmail.com>
2025-02-16 19:28:58 +0200

    test

--- src/routes/reports_nodes_Router.ts
+++ src/routes/reports_nodes_Router.ts
@@ -6,23 +6,23 @@ const router = express.Router();
 
 router
   .route("/")
-  .get(report_nodes_controller.get_root_nodes)
-  .post(report_nodes_controller.post_nodes);
+  .get(authController.authenticate_jwt_token, report_nodes_controller.get_root_nodes)
+  .post(authController.authenticate_jwt_token, report_nodes_controller.post_nodes);
 
 
 router
   .route("/BlackBox")
-  .post(report_nodes_controller.BlackBox_Scripts);
+  .post(authController.authenticate_jwt_token, report_nodes_controller.BlackBox_Scripts);
 
 router
   .route("/:id")
-  .get(report_nodes_controller.navigate_tree_data)
-  .delete(report_nodes_controller.delete_node);
+  .get(authController.authenticate_jwt_token, report_nodes_controller.navigate_tree_data)
+  .delete(authController.authenticate_jwt_token, report_nodes_controller.delete_node);
 
 router
   .route("/Rules/:id")
-  .get(report_nodes_controller.get_rules)
-  .post(report_nodes_controller.post_rules);
+  .get(authController.authenticate_jwt_token, report_nodes_controller.get_rules)
+  .post(authController.authenticate_jwt_token, report_nodes_controller.post_rules);
 
 
 export default router;

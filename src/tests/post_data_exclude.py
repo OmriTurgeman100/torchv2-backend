3d4cb5be68b149bb0749379ecb6b70db64379464
omri <omrigamer100@gmail.com>
2025-02-19 22:14:34 +0200

    test

--- src/tests/post_data_exclude.py
+++ src/tests/post_data_exclude.py
@@ -27,7 +27,7 @@ def post_data_to_reports_exclude():
 
                     print(response.status_code)
 
-                    shutil.rmtree("__pycache__", ignore_errors=True)
+                    
 
     except Exception as e:
         print(e)

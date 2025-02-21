5f01951d3a56dd478692bb5b6475665f0c3122ae
omri <omrigamer100@gmail.com>
2025-02-21 04:53:07 +0200

    test

--- src/utils/UpdateTreeTime.ts
+++ src/utils/UpdateTreeTime.ts
@@ -8,7 +8,7 @@ export const expired_tree_evaluation = async (): Promise<void> => {
       "select * from nodes where time < now() - interval '1 min';"
     );
 
-    console.log(expired_node.rows)
+    console.log(expired_node.rows);
 
     for (const node of expired_node.rows) {
       await pool.query(
@@ -24,7 +24,7 @@ export const expired_tree_evaluation = async (): Promise<void> => {
           [expired_node_parent]
         );
 
-        console.log(node.rows)
+        console.log(node.rows); // * test
 
         const update_recursion = await pool.query(
           "update nodes set status = 'expired' where node_id = $1;",

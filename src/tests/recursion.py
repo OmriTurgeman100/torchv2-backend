50f6d755891c66d0922836c44455524d4df78c09
omri <omrigamer100@gmail.com>
2025-02-22 01:39:55 +0200

    test

--- src/tests/recursion.py
+++ src/tests/recursion.py
@@ -6,11 +6,11 @@ def recursion():
 
         token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX25hbWUiOiJhZG1pbiIsInVzZXJfaWQiOjE0NiwidXNlcl9yb2xlIjoiZ3Vlc3QiLCJpYXQiOjE3Mzk5OTE1NjQsImV4cCI6MTc0MDI1MDc2NH0.tXSjkBMG_RAQPcQAVzS6Z27FDkevwwWW-0zCg9E3l2Q'
 
-
-
         response = requests.get("http://localhost:3000/api/v1/reports/nodes/173", headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'})
 
-        print(response.text)
+        data = response.json()
+
+        print(data["node"])
 
        
 
